c1816c987f5231fce4ce508097740271f4ab31ab
MalachiTimothyPhillips <malachiphillips@ou.edu>
2017-04-30 19:21:10 -0500

    Implemented methods inside the TextView

--- src/TextView.java
+++ src/TextView.java
@@ -1,7 +1,10 @@
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.swing.*;
 
@@ -29,7 +32,7 @@ public class TextView implements ActionListener
 	/** List of <code>NewsMedia</code> associated with the stories **/
 	private List<NewsMedia> newsMedia = new ArrayList<NewsMedia>();
 	
-	/** List of <code>SortCriterion</code> assocaited with the stories **/
+	/** List of <code>SortCriterion</code> associated with the stories **/
 	private List<SortCriterion> sortCriteria  = new ArrayList<SortCriterion>();
 	
 	/** <code>String</code> to hold the list of stories **/
@@ -42,7 +45,7 @@ public class TextView implements ActionListener
 	private JTextArea jtaNewsStoryList;
 	
 	/** <code>JScrollPane</code> to place the <code>JTextArea</code> inside **/
-	private JScrollPane jspNewsStoryList = new JScrollPane(jtaNewsStoryList);
+	private JScrollPane jspNewsStoryList;
 	
 	/** <code>JTextArea</code> to hold the summary lines **/
 	private JTextArea jtaSummaryLine;
@@ -58,24 +61,167 @@ public class TextView implements ActionListener
 	 *   List of <code>StoryCriterion</code> used to sort the storiese
 	 *   
 	 */
-	public TextView(){
-		//TODO: Construct the things here
+	public TextView(NewsMakerModel newsMakerModel,
+			        List<NewsMedia> newsMedia,
+			        List<SortCriterion> sortCriteria){
+		
+		// set the newsMedia, sortCriteria
+		this.newsMedia = newsMedia;
+		this.sortCriteria = sortCriteria;
+		
+		// construct the title
+	    String title = constructTitle();
+	    
+	    // construct everything else
+	    constructNewsStoriesAndSummary();
+	    
+	    // create the graphics!
+	    
+	    jtaSummaryLine = new JTextArea(summaryLine);
+	    jtaNewsStoryList = new JTextArea(listOfStories);
+	    jspNewsStoryList = new JScrollPane(jtaNewsStoryList);
+	    jfText.setTitle(title);
+	    jfText.setLayout(new GridLayout(1,2));
+	    jfText.add(jtaSummaryLine);
+	    jfText.add(jspNewsStoryList);
+	    
 	}
 	
 	/**
 	 * Private helper method for constructing both the
 	 * news stories and the summary
+	 * 
+     * This method takes a news maker and turns its list of news stories into a
+     * <code>String</code> formated for display to the user or saving to a text
+     * file. At the end it includes a line summarizing the number of stories
+     * found, the number of different news sources in which these stories were
+     * published, the total length of these stories, and the number of different
+     * topics found.
+     * <P>
+     * The summary line will have a slightly different format depending on media
+     * type specified.
+     * </P>
+     * <dl>
+     * <dt>Newspapers and/or online news:</dt>
+     * <dd>Number of Stories: <i>n1</i>; Number of Sources: <i>n2</i>; Number of
+     * Words: <i>n3</i>; Number of Topics: <i>n4</i>; Number of Subjects:
+     * <i>n5</i></dd>
+     * <dt>TV news:</dt>
+     * <dd>Number of Stories: <i>n1</i>; Number of Sources: <i>n2</i>; Seconds:
+     * <i>n3</i>; Number of Topics: <i>n4</i>; Number of Subjects:
+     * <i>n5</i></dd>
+     * <dt>Mixed (TV plus either or both of newspapers and online news):</dt>
+     * <dd>Number of Stories: <i>n1</i>; Number of Sources: <i>n2</i>; Number of
+     * Word Equivalents: <i>n3</i>; Number of Topics: <i>n4</i>; Number of
+     * Subjects: <i>n5</i></dd>
+     * </dl>
+	 * 
 	 */
 	private void constructNewsStoriesAndSummary(){
-		//TODO: Construct the News stories and summary
+		// start by extracting out all of the news stories of the correct type
+		// convert the list of news media types into their respective class names
+		ArrayList<String> mediaTypes = new ArrayList<String>();
+		for (int i = 0 ; i < newsMedia.size(); ++i){
+			switch(newsMedia.get(i).toString()){
+			case "TV":
+				mediaTypes.add("TVNewsStory");
+				break;
+			case "Online":
+				mediaTypes.add("OnlineNewsStory");
+				break;
+			case "Newspaper":
+				mediaTypes.add("NewspaperStory");
+				break;
+			}	
+		}
+		
+		// now, pull the newsStories from the newsMakerModel object
+		DefaultListModel<NewsStory> news = newsMakerModel.getNewsStoryListModel().getNewsStories();
+		
+		// the actual news stories that we care about
+		ArrayList<NewsStory> relevantStories = new ArrayList<NewsStory>();
+		
+		for (int i = 0 ; i < news.size(); ++i){
+			NewsStory current = news.getElementAt(i);
+			
+			// get the name of the type
+			String type = current.getClass().getName();
+			if (mediaTypes.contains(type)){ // type is included, so add the story!
+				relevantStories.add(current);
+			}
+		} // once this is done, we care about each and every story left in the list
+		
+		// Of course, we need some statistics for the summary line -- store in here!
+		// Set for sources
+		Set<String> sourceSet = new TreeSet<String>();
+		
+		// Set for subjects
+		Set<String> subjectSet = new TreeSet<String>();
+		
+		// Set for topics
+		Set<String> topicSet = new TreeSet<String>();
+		
+		// counters
+		int secondLength = 0;
+		int wordLength = 0;
+		
+		// iterate over the stories
+		for (NewsStory n : relevantStories){
+			// add in the subject, source and topic
+			sourceSet.add(n.getSource());
+			subjectSet.add(n.getSubject());
+			topicSet.add(n.getTopic());
+			wordLength += n.getLengthInWords();
+			secondLength += n.getLength();
+			
+			// add onto the listOfStory this current story
+			listOfStories += UserInterface.convertToOutputFormat(n, newsMedia);
+		} // statistics collected
+		
+		// now, make the summary line
+		summaryLine = "Number of Stories: " + relevantStories.size();
+		summaryLine += "; Number of Sources: " + sourceSet.size();
+		if(mediaTypes.contains("TVNewsStory") && (mediaTypes.contains("NewspaperStory") || mediaTypes.contains("OnlineNewsStory"))){
+			summaryLine += "; Number of Word Equivalents: " + wordLength;
+		} if(!mediaTypes.contains("TVNewsStory")){
+			summaryLine += "; Number of Words: " + wordLength;
+		} else {
+			summaryLine += "; Number of Seconds: " + secondLength;
+		}
+		
+		summaryLine += "; Number of Topics: " + topicSet.size();
+		summaryLine += "; Number of Subjects: " + subjectSet.size();
+		
+		// summary line is finished, listOfStory is finished
+		// end here
+				
 	}
 	
 	/**
 	 * Private helper method for constructing the title
 	 * for the text view
 	 */
-	private void constructTitle(){
-		//TODO: construct the title
+	private String constructTitle(){
+		// title consists of:
+		String title = "NewsMaker Name:";
+		title += newsMakerModel.getName();
+		title += "; Sorted by: ";
+		for (int i = 0 ; i < sortCriteria.size()-1; ++i){
+			title += sortCriteria.get(i).toString();
+			title += ", ";
+		}
+		title += sortCriteria.get(sortCriteria.size()-1);
+		title += "; Media types: ";
+		for (int i = 0 ; i < newsMedia.size()-1; ++i){
+			title += newsMedia.get(i).toString();
+			title += ", ";
+		}
+		title += newsMedia.get(sortCriteria.size()-1);
+		title += ".";
+		
+		return title;
+		//TODO: Need to wait on the docs to verify this is correct
+					
 	}
 	
 	/**
