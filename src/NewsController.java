8c37861e2e9faab153dad23f0c80912fae7707c3
MalachiTimothyPhillips <malachiphillips@ou.edu>
2017-05-03 23:14:29 -0500

    Fixed several issues with adding in stories in the AddEditNewsStoryView

--- src/NewsController.java
+++ src/NewsController.java
@@ -1286,6 +1286,11 @@ public class NewsController
 				String newsMakerName2 = (String) addEditNewsStoryView.jcbNewsStoryNewsMaker2.getSelectedItem();
 				NewsMakerModel newsMaker2 = newsDataBaseModel.getNewsMakerListModel().getExactMatch(newsMakerName2);
 				
+				// check if these are the same (except for none)
+				if(newsMakerName1.equals(newsMakerName2) && !"None".equals(newsMakerName1)){
+					return; // halt the process here
+				}
+				
 				PartOfDay pod = (PartOfDay) addEditNewsStoryView.jcbNewsStoryPartOfDay.getSelectedItem();
 				
 				String source = (String) addEditNewsStoryView.jcbNewsStorySource.getSelectedItem();
@@ -1333,18 +1338,26 @@ public class NewsController
 					break;
 				}
 				
+				// check if the news makers exist, if not, add them
+				if(!newsDataBaseModel.getNewsMakerListModel().contains(newsMaker1)){
+					// create new NewsMakerModel
+					newsDataBaseModel.addNewsMakerModel(new NewsMakerModel(newsMakerName1));
+				}
+				if(!newsDataBaseModel.getNewsMakerListModel().contains(newsMaker2) && !"None".equals(newsMakerName2)){
+					// create new NewsMakerModel
+					newsDataBaseModel.addNewsMakerModel(new NewsMakerModel(newsMakerName2));
+				}
+				
 				// add news story
 				newsDataBaseModel.addNewsStory(news);	
 				
 				// add to relevant news makers
-				NewsMakerModel newsMakerFirst = newsDataBaseModel.getNewsMakerListModel().get(news.getNewsMaker1());
-				NewsMakerModel newsMakerSecond = newsDataBaseModel.getNewsMakerListModel().get(news.getNewsMaker2());
+				newsDataBaseModel.getNewsMakerListModel().getExactMatch(newsMakerName1).addNewsStory(news);
 				
-				newsMakerFirst.addNewsStory(news);
-				newsMakerSecond.addNewsStory(news);
-				
-				// TODO: Add in check to see that the newsmakers are not the same person
-				// unless, however, it is none
+				// check for if it is none
+				if(!"None".equals(newsMakerName2)){
+					newsDataBaseModel.getNewsMakerListModel().getExactMatch(newsMakerName2).addNewsStory(news);
+				}
 				
 				viewDialog.dispose(); // once added, throw out
 				
@@ -1427,8 +1440,11 @@ public class NewsController
 				newsMakerFirst = newsDataBaseModel.getNewsMakerListModel().get(news.getNewsMaker1());
 				newsMakerSecond = newsDataBaseModel.getNewsMakerListModel().get(news.getNewsMaker2());
 				
-				newsMakerFirst.addNewsStory(news);
-				newsMakerSecond.addNewsStory(news);
+				//newsMakerFirst.addNewsStory(news);
+				//newsMakerSecond.addNewsStory(news);
+				newsMaker1.addNewsStory(news);
+				newsMaker2.addNewsStory(news);
+				
 				
 				// TODO: Add in check to see that the newsmakers are not the same person
 				// unless, however, it is none
